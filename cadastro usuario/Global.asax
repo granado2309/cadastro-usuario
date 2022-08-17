81838a69237ec174096d41a2861bc517ce446c18
Everton Granado Silva <granado2309@gmail.com>
2022-08-17 10:24:30 -0300

    Adicionar arquivos de projeto.

---
+++ cadastro usuario/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cadastro_usuario.MvcApplication" Language="C#" %>
---
+++ cadastro usuario/Models/contexto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace cadastro_usuario.Models
+{
+    public class context: DbContext
+    {
+        public DbSet<pessoa> Pessoas { get; set; }
+    }
+}
